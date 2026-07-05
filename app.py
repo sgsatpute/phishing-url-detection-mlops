97ec3767edb873730c964aaee2539949d028537f
Saurav Satpute <sgsatpute2005@gmail.com>
2026-07-05 16:35:17 +0530

    Fix TemplateResponse argument order for updated Starlette API

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ async def predict_route(request: Request, file: Annotated[UploadFile, File()] = 
         table_html = df.to_html(classes="table table-striped")
         # print(table_html)
         return templates.TemplateResponse(
-            "table.html", {"request": request, "table": table_html},
+            request, "table.html", {"table": table_html},
         )
 
     except Exception as e:
